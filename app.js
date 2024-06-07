816cea5f7509c70c8122e83ff4d616f388d0a93b
negraop <pedro.negrao12@gmail.com>
2024-06-06 23:32:42 -0300

    aula 1

---
+++ app.js
@@ -0,0 +1,7 @@
+alert("Boas Vindas ao Jogo do Número Secreto!");
+let numeroSecreto = 5;
+let chute = prompt("Escolha um número entre 1 e 10");
+
+if (chute == numeroSecreto) {
+    console.log("Isso aí! Você descobriu o número secreto");
+} 
