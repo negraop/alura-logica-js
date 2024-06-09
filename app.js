3afd55f057deaf2bd990b5d42230c27a65c8e23f
negraop <pedro.negrao12@gmail.com>
2024-06-09 11:12:24 -0300

    aula 3

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 alert("Boas Vindas ao Jogo do Número Secreto!");
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt("Escolha um número entre 1 e 10");
+let chute;
+let tentativas = 0
 
-// se chute for igual ao numeroSecreto
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você errou =(');
+// enquanto chute for diferente do numeroSecreto
+while (chute != numeroSecreto) {
+    chute = prompt("Escolha um número entre 1 e 10");
+    tentativas++;
+
+    // se chute for igual ao numeroSecreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
 }
+
