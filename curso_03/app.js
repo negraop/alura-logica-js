7bcf3a25ba46133ab4d728dc31d9ec475bfc028f
negraop <pedro.negrao12@gmail.com>
2024-06-11 23:17:28 -0300

    este commit foi escrito usando o git integrado no VSCode
    Com ele eu também consigo escrever o título do commit, e eu separar o titulo do corpo com uma linha em branco, ele considera este texto aqui como sendo o corpo do commit, permitindo que eu insira um maior detalhamento do commit.

--- curso_03/app.js
+++ curso_03/app.js
@@ -1,7 +1,7 @@
 let listaDeNumerosSorteados = [];
 let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
-let tentativas = 1; 
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
